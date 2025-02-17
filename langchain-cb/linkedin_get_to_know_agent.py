5301a321f0ca517927656691a1948bba9335b989
rpguptai <rpgupta.ai@gmail.com>
2025-02-17 16:29:50 +0100

    added linkedin lookup agent

--- langchain-cb/linkedin_get_to_know_agent.py
+++ langchain-cb/linkedin_get_to_know_agent.py
@@ -2,37 +2,36 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama
-
-# from langchain_openai import ChatOpenAI
-import os
+from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin_mock import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello World")
-    print(os.environ["TEST_API_KEY"])
+
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
-
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
-    # llm = ChatOpenAI(temperature=0)
-    # llm = ChatOllama(model="llama3")deepseek-r1:7b
-    llm = ChatOllama(model="deepseek-r1:7b")
-    # chain = summary_prompt_template | llm
+    llm = ChatOllama(temperature=0,model="deepseek-r1:7b")
+
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
-    )
 
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco")
